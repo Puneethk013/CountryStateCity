4079715463f94bcf70c69ab1d8a4c59096944c01
Puneeth Kumar <puneeth.kumar@zensar.com>
2021-02-08 08:55:23 +0530

    Add project files.

---
+++ Country.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Country.aspx.cs" Inherits="CountryStateCity.Country" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Select Your Country Name:</div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" Height="53px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="295px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        Select Your State Name:<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" Height="45px" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" Width="295px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        Select Your City Name:<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList3" runat="server" AutoPostBack="True" Height="38px" OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged" Width="295px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Country.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace CountryStateCity
+{
+    public partial class Country : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+            {
+                if (!Page.IsPostBack)
+                {
+                    cn.Open();
+                    using (SqlCommand cmd = new SqlCommand("Select * from CountryTab", cn))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                        {
+                           
+                            if(dr.HasRows)
+                            {
+                                DataTable dt = new DataTable();
+                                dt.Load(dr);
+                                DropDownList1.DataSource = dt;
+                                DropDownList1.DataValueField = "cid";
+                                DropDownList1.DataTextField = "CountryName";
+                                DropDownList1.DataBind();
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+            {
+                var a = DropDownList1.SelectedValue;  
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("Select * from StateTable where Countryid=" + a + "", cn))
+                {
+                    using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        if (dr.HasRows)
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(dr);
+                            DropDownList2.DataSource = dt;
+                            DropDownList2.DataValueField = "sid";
+                            DropDownList2.DataTextField = "StateName";
+                            DropDownList2.DataBind();
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString()))
+            {
+               
+                var a = DropDownList2.SelectedValue;
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("Select * from CityTable where sid=" + a + "", cn))
+                {
+                    using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                    {
+                        if (dr.HasRows)
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(dr);
+                            DropDownList3.DataSource = dt;
+                            DropDownList3.DataValueField = "CityId";
+                            DropDownList3.DataTextField = "CityName";
+                            DropDownList3.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
